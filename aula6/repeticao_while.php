f77b56671d994ad6c5f67e192f7edb2e25a623f6
Adrianzildo <125364660+Adrianzildo@users.noreply.github.com>
2023-03-07 08:44:11 -0300

    repetição while

---
+++ aula6/repeticao_while.php
@@ -0,0 +1,22 @@
+<h2>Estrutura de repetição While</h2>
+<hr>
+<p>
+     Utilizada para repetir um conjunto de instruções.
+</p>
+
+<strong>Exemplo</strong>
+
+<pre>
+    While(condição){
+        //comandos a serem repetidos.
+    }
+</pre>
+
+<?php
+   $contador = 0;
+   while($contador <= 10){
+      echo "Olá pessoal $contador<br>";
+      $contador++;
+   }
+
+?>
